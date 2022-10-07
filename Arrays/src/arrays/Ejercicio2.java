9ed09ab1d7e1ac242654b2c913fd02407432ecf3
cristian miguel marin barrera <cristian_enano_99@hotmail.com>
2022-10-06 22:32:13 -0500

    Avance de ejericio 2

--- Arrays/src/arrays/Ejercicio2.java
+++ Arrays/src/arrays/Ejercicio2.java
@@ -9,8 +9,22 @@ public class Ejercicio2 {
     
     //ccc
     public void permutaciones(String cadena1, String cadena2){
+        if(cadena1.length() > cadena2.length()){
+            int rangoA = 0;
+            for(int i = cadena2.length(); i < cadena1.length(); i++){
+                String rango = cadena1.substring(rangoA, i);
+                if(rango.equals(cadena2)){
+                    
+                }
+                rangoA++;
+            }
+        }else if(cadena1.length() < cadena2.length()){
+            
+        }else{
+            
+        }
         for (int i = 0; i < 10; i++) {
-
+            
         }
     }
 }
